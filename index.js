0d196de79521280cd8434ecc2880a144313b5818
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-27 10:59:39 -0700

    add name of sender to msgs

--- index.js
+++ index.js
@@ -9,4 +9,21 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+// define socket listeners
+// connection and disconnect are built-in understood events
+io.on('connection', function(socket) {
+  console.log('a new connection has been established');
+
+  socket.on('chat msg', function(msg) {
+    // what to do if a 'chat msg' is recieved from a client
+    // in this case, tell all other clients about it
+    io.emit('chat msg', msg);
+  });
+
+  // if any action on disconnect is required, put it here
+  socket.on('disconnect', function() {
+    console.log('a connection has been lost');
+  });
+});
+
 http.listen(3000);
